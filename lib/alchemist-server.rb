52d7496879a859a3cb81bbf13f879407570709f6
David Vollbracht <david@flipstone.com>
2012-10-05 16:22:28 -0400

    Dev Scaffold

---
+++ lib/alchemist-server.rb
@@ -0,0 +1,9 @@
+require "alchemist-server/version"
+
+module Alchemist
+  module Server
+    def self.one_shot(world_file, command)
+      "Got Command 2"
+    end
+  end
+end
---
+++ lib/alchemist-server/version.rb
@@ -0,0 +1,5 @@
+module Alchemist
+  module Server
+    VERSION = "0.0.1"
+  end
+end
